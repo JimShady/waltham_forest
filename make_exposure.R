8d3a45f053b7d76eed4b4c58a4bcc5569fc3cac9
James <james.david.smith@gmail.com>
2018-05-10 10:36:41 +0100

    air quality layers

---
+++ make_exposure.R
@@ -0,0 +1,209 @@
+rm(list = ls())
+
+## Load libraries
+library("googleway")
+library("gepaf")
+library("sp")
+library('raster')
+library('ggplot2')
+library('geosphere')
+
+setwd("/home/james/mounts/botanix-botanixg/Projects/WalthamForest/Modelling/re_sampling/")
+
+exposure_routes <- read.csv('/home/james/mounts/botanix-botanixg/Projects/WalthamForest/Modelling/re_sampling/exposure_journeys.csv',
+                            stringsAsFactors = F)
+
+exposure_routes[exposure_routes$via_array == '',]$via_array <- NA
+
+for (i in 1:nrow(exposure_routes)) {
+
+# Journey parameters
+start_lat         <- exposure_routes[i,]$start_lat
+start_lon         <- exposure_routes[i,]$start_lon
+if (!is.na(exposure_routes[i,]$via_array)) {via_array         <- as.list(strsplit(as.character(exposure_routes[i,]$via_array), ";")[[1]]) } else {via_array <- NA}
+end_lat           <- exposure_routes[i,]$end_lat
+end_lon           <- exposure_routes[i,]$end_lon
+mode              <- as.character(exposure_routes[i,]$mode)
+start_time        <- Sys.time() + 1000
+seggregated       <- exposure_routes[i,]$seggregated
+
+## take account of where in the road the journey is taking place
+if (mode == 'walk')                           {offset <- 6}
+if (mode == 'bicycle' & seggregated == 'no')  {offset <- 2}
+if (mode == 'bicycle' & seggregated == 'yes') {offset <- 4}
+
+
+# EPSG strings we might need
+latlong                 <- "+init=epsg:4326"
+ukgrid                  <- "+init=epsg:27700"
+google_projected        <- "+init=epsg:3857 +proj=merc +a=6378137 +b=6378137 +lat_ts=0.0 +lon_0=0.0 +x_0=0.0 +y_0=0 +k=1.0 +units=m +nadgrids=@null +no_defs"
+
+# Take the start/end data and get a route from Google
+if ('walk' %in% mode)     { source("/home/james/mounts/James/drayson/routing/walking.R") }
+if ('bicycle' %in% mode)  { source("/home/james/mounts/James/drayson/routing/cycling.R") }
+
+rm(start_lat, start_lon, end_lat, end_lon, via_array)
+
+if ('walk' %in% mode) {
+  result <- walk_result
+  rm(walk_result)
+  duration <- walk_duration
+  rm(walk_duration)
+} else {
+  result <- cycle_result
+  rm(cycle_result)
+  duration <- cycle_duration
+  rm(cycle_duration)
+}
+
+##### In here is where we calculate the offset for where the journey is taking place compared to the centre of the road
+
+for (i in 2:nrow(result)) {
+  
+}
+
+bearing(result[2,], result[3,])
+
+
+#####
+
+coordinates(result) <- ~lon + lat
+proj4string(result) = CRS(latlong)
+result <- spTransform(result, ukgrid)
+
+## This makes a spatialline from the points for the result
+x <- lapply(split(result, result$id), function(x) Lines(list(Line(coordinates(x))), result$id[1L]))
+lines <- SpatialLines(x)
+data <- data.frame(id = unique(result$id))
+rownames(data) <- data$id
+result <- SpatialLinesDataFrame(lines, data)
+proj4string(result) = CRS(ukgrid)
+rm(x, lines, data)
+
+
+## How many minutes long was the bicycle and walk journeys
+
+timeslots                <- data.frame(id = unique(result$id), time = seq.POSIXt(start_time, start_time + duration, "min"))
+
+## Now want to split the line into a point per minute
+
+result                   <- data.frame(spsample(result, 
+                                                nrow(timeslots),
+                                                type="regular"))
+
+## Now some harmonisation stuff to get the car, walk and bus all the same format so can join them together
+result$id                 <- exposure_routes[i,]$journey_id
+result$mode               <- mode
+result$line               <- NA
+result                    <- cbind(result, timeslots)
+result                    <- result[,c('id', 'mode', 'line', 'time', 'lon', 'lat')]
+coordinates(result)       <- ~lon + lat
+proj4string(result)       <- CRS(ukgrid)
+
+
+# Now get our pollutant files
+
+## 2013 concentration maps
+wf_2013_no2              <- raster('/home/james/mounts/botanix-botanixg/Projects/WalthamForest/Modelling/re_sampling/5m_outputs/no2_2013.asc')
+crs(wf_2013_no2)         <- CRS(ukgrid)
+wf_2013_nox              <- raster('/home/james/mounts/botanix-botanixg/Projects/WalthamForest/Modelling/re_sampling/5m_outputs/nox_2013.asc')
+crs(wf_2013_nox)         <- CRS(ukgrid)
+wf_2013_pm10             <- raster('/home/james/mounts/botanix-botanixg/Projects/WalthamForest/Modelling/re_sampling/5m_outputs/pm10_2013.asc')
+crs(wf_2013_pm10)        <- CRS(ukgrid)
+wf_2013_pm25             <- raster('/home/james/mounts/botanix-botanixg/Projects/WalthamForest/Modelling/re_sampling/5m_outputs/pm25_2013.asc')
+crs(wf_2013_pm25)        <- CRS(ukgrid)
+
+## 2021 concentration maps without schemes
+wf_2021_no2              <- raster('/home/james/mounts/botanix-botanixg/Projects/WalthamForest/Modelling/re_sampling/5m_outputs/no2_2021.asc')
+crs(wf_2021_no2)         <- CRS(ukgrid)
+wf_2021_nox              <- raster('/home/james/mounts/botanix-botanixg/Projects/WalthamForest/Modelling/re_sampling/5m_outputs/nox_2021.asc')
+crs(wf_2021_nox)         <- CRS(ukgrid)
+wf_2021_pm10             <- raster('/home/james/mounts/botanix-botanixg/Projects/WalthamForest/Modelling/re_sampling/5m_outputs/pm10_2021.asc')
+crs(wf_2021_pm10)        <- CRS(ukgrid)
+wf_2021_pm25             <- raster('/home/james/mounts/botanix-botanixg/Projects/WalthamForest/Modelling/re_sampling/5m_outputs/pm25_2021.asc')
+crs(wf_2021_pm25)        <- CRS(ukgrid)
+
+## 2021 concentration maps with schemes
+wf_2021_no2_s            <- raster('/home/james/mounts/botanix-botanixg/Projects/WalthamForest/Modelling/re_sampling/5m_outputs/no2_2021.asc')
+crs(wf_2021_no2_s)       <- CRS(ukgrid)
+wf_2021_nox_s            <- raster('/home/james/mounts/botanix-botanixg/Projects/WalthamForest/Modelling/re_sampling/5m_outputs/nox_2021.asc')
+crs(wf_2021_nox_s)       <- CRS(ukgrid)
+wf_2021_pm10_s           <- raster('/home/james/mounts/botanix-botanixg/Projects/WalthamForest/Modelling/re_sampling/5m_outputs/pm10_2021.asc')
+crs(wf_2021_pm10_s)      <- CRS(ukgrid)
+wf_2021_pm25_s           <- raster('/home/james/mounts/botanix-botanixg/Projects/WalthamForest/Modelling/re_sampling/5m_outputs/pm25_2021.asc')
+crs(wf_2021_pm25_s)      <- CRS(ukgrid)
+
+rm(timeslots, duration, start_time )
+
+# Extract the concentrations from the Raster files
+
+# 2013
+result@data$no2_2013           <- extract(wf_2013_no2, result@coords)
+result@data$pm25_2013          <- extract(wf_2013_pm25, result@coords)
+result@data$pm10_2013          <- extract(wf_2013_pm10, result@coords)
+result@data$nox_2013           <- extract(wf_2013_nox, result@coords)
+
+# 2021 without scheme
+result@data$no2_2021           <- extract(wf_2021_no2, result@coords)
+result@data$pm25_2021          <- extract(wf_2021_pm25, result@coords)
+result@data$pm10_2021          <- extract(wf_2021_pm10, result@coords)
+result@data$nox_2021           <- extract(wf_2021_nox, result@coords)
+
+# 2021 with scheme
+result@data$no2_2021_s         <- extract(wf_2021_no2, result@coords)
+result@data$pm25_2021_s        <- extract(wf_2021_pm25, result@coords)
+result@data$pm10_2021_se       <- extract(wf_2021_pm10, result@coords)
+result@data$nox_2021_s         <- extract(wf_2021_nox, result@coords)
+
+## Calculate some cumulative exposure data for each pollutant by transport mode
+
+# 2013
+result@data$cumulative_no2_2013  <- cumsum(result@data$no2_2013)
+result@data$cumulative_pm25_2013 <- cumsum(result@data$pm25_2013)
+result@data$cumulative_pm10_2013 <- cumsum(result@data$pm10_2013)
+result@data$cumulative_nox_2013  <- cumsum(result@data$nox_2013)
+
+# 2021 without scheme
+result@data$cumulative_no2_2021  <- cumsum(result@data$no2_2021)
+result@data$cumulative_pm25_2021 <- cumsum(result@data$pm25_2021)
+result@data$cumulative_pm10_2021 <- cumsum(result@data$pm10_2021)
+result@data$cumulative_nox_2021  <- cumsum(result@data$nox_2021)
+
+# 2021 with scheme
+result@data$cumulative_no2_2021_s  <- cumsum(result@data$no2_2021_s)
+result@data$cumulative_pm25_2021_s <- cumsum(result@data$pm25_2021_s)
+result@data$cumulative_pm10_2021_s <- cumsum(result@data$pm10_2021_s)
+result@data$cumulative_nox_2021_s  <- cumsum(result@data$nox_2021_S)
+
+## Put results back into the starting table
+exposure_routes[i,'mean_no2_2013']       <- mean(result@data$no2_2013)
+exposure_routes[i,'mean_pm25_2013']      <- mean(result@data$pm25_2013)
+exposure_routes[i,'mean_pm10_2013']      <- mean(result@data$pm10_2013)
+exposure_routes[i,'mean_nox_2013']       <- mean(result@data$nox_2013)
+
+exposure_routes[i,'mean_no2_2021']       <- mean(result@data$no2_2021)
+exposure_routes[i,'mean_pm25_2021']      <- mean(result@data$pm25_2021)
+exposure_routes[i,'mean_pm10_2021']      <- mean(result@data$pm10_2021)
+exposure_routes[i,'mean_nox_2021']       <- mean(result@data$nox_2021)
+
+exposure_routes[i,'mean_no2_2021_s']       <- mean(result@data$no2_2021_s)
+exposure_routes[i,'mean_pm25_2021_s']      <- mean(result@data$pm25_2021_s)
+exposure_routes[i,'mean_pm10_2021_s']      <- mean(result@data$pm10_2021_s)
+exposure_routes[i,'mean_nox_2021_s']       <- mean(result@data$nox_2021_s)
+
+exposure_routes[i,'total_no2_2013']       <- mean(result@data$cumulative_no2_2013)
+exposure_routes[i,'total_pm25_2013']      <- mean(result@data$cumulative_pm25_2013)
+exposure_routes[i,'total_pm10_2013']      <- mean(result@data$cumulative_pm10_2013)
+exposure_routes[i,'total_nox_2013']       <- mean(result@data$cumulative_nox_2013)
+
+exposure_routes[i,'total_no2_2021']       <- mean(result@data$cumulative_no2_2021)
+exposure_routes[i,'total_pm25_2021']      <- mean(result@data$cumulative_pm25_2021)
+exposure_routes[i,'total_pm10_2021']      <- mean(result@data$cumulative_pm10_2021)
+exposure_routes[i,'total_nox_2021']       <- mean(result@data$cumulative_nox_2021)
+
+exposure_routes[i,'total_no2_2021_s']       <- mean(result@data$cumulative_no2_2021_s)
+exposure_routes[i,'total_pm25_2021_s']      <- mean(result@data$cumulative_pm25_2021_s)
+exposure_routes[i,'total_pm10_2021_s']      <- mean(result@data$cumulative_pm10_2021_s)
+exposure_routes[i,'total_nox_2021_s']       <- mean(result@data$cumulative_nox_2021_s)
+
+}
